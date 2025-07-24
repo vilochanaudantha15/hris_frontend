7f8a521950abb14931c3a1acb5481bbcf776fbc1
Vilochana_Udantha <138508724+vilochanaudantha15@users.noreply.github.com>
2025-07-24 16:07:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ import NonEfinalAttendance from './components/NonEfinalAttendance';
 import Payroll from './components/Payroll';
 
 // Define API base URL
-const API_BASE_URL = '/api/employ';
+const API_BASE_URL = '/employ';
 
 // ProtectedRoute component to restrict access to authenticated users with valid token
 function ProtectedRoute({ children }) {
